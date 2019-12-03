bcde225813fec4a4a14a157436467a3df5ef01b7
danielbekhter <57594319+danielbekhter@users.noreply.github.com>
2019-12-03 03:05:23 +0100

    OnPointer interactions set up for the doors

---
+++ Assets/TiltMazeVR/Scripts/DoorInteraction.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorInteraction : MonoBehaviour, IMixedRealityPointerHandler
+{
+
+    private DoorController contr;
+    private bool state = false;
+
+    // Use this for initialization
+    void Start () {
+        contr = gameObject.GetComponent<DoorController>();
+        contr.DoorOpen = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        if (contr.DoorOpen == false)
+        {
+            contr.DoorOpen = true;
+        }
+        else
+        {
+            contr.DoorOpen = false;
+        };
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        contr.DoorOpen = contr.DoorOpen;
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        if (contr.DoorOpen == false)
+        {
+            contr.DoorOpen = true;
+        }
+        else
+        {
+            contr.DoorOpen = false;
+        };
+    }
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        if (contr.DoorOpen == false)
+        {
+            contr.DoorOpen = true;
+        }
+        else
+        {
+            contr.DoorOpen = false;
+        };
+    }
+}
